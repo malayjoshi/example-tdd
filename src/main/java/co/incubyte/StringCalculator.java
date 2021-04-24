267892f0c06e7ae5c3f9699ddfd476b178406e9a
malayjoshi <malayace.joshi@gmail.com>
2021-04-24 16:11:32 +0530

    1- Refactored code to pass all test cases. Till now 3 TCs.

--- src/main/java/co/incubyte/StringCalculator.java
+++ src/main/java/co/incubyte/StringCalculator.java
@@ -6,8 +6,15 @@ public class StringCalculator {
 		
 		if(numbers.length()==0)
 			sum=0;
-		else if( numbers.length()==1 )
-			sum=Integer.parseInt(numbers);
+		else if( numbers.length()>0 ) {
+			if( numbers.length()==1 )
+				sum=Integer.parseInt(numbers);
+			else if(numbers.length()>1 ){
+				sum= Integer.parseInt( numbers.split(",")[0] )+
+						Integer.parseInt( numbers.split(",")[1] );
+			}
+				
+		}
 		
 		return sum;
 		
