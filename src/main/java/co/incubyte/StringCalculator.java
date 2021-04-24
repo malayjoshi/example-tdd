c537ae5386ce9871cd734c04860cf60daecf5745
malayjoshi <malayace.joshi@gmail.com>
2021-04-24 16:38:17 +0530

    1- refactored code to handle multiple numbers with comma delimiter.

--- src/main/java/co/incubyte/StringCalculator.java
+++ src/main/java/co/incubyte/StringCalculator.java
@@ -2,17 +2,13 @@ package co.incubyte;
 
 public class StringCalculator {
 	public static int add(String numbers) {
-		int sum=-1;
+		int sum=0;
 		
-		if(numbers.length()==0)
-			sum=0;
-		else if( numbers.length()>0 ) {
-			if( numbers.length()==1 )
-				sum=Integer.parseInt(numbers);
-			else if(numbers.length()>1 ){
-				sum= Integer.parseInt( numbers.split(",")[0] )+
-						Integer.parseInt( numbers.split(",")[1] );
-			}
+		if( numbers.length()>0 ) {
+			
+			String[] splittedNumbers=numbers.split(",");
+			for(int i=0;i<splittedNumbers.length;i++ )
+				sum+=Integer.parseInt( splittedNumbers[i] );
 				
 		}
 		
